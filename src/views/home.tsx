b14c8142fac61b40aad63b6d26a0f06e8b8cea90
timers <timershamlet@outlook.com>
2021-03-12 11:29:38 +0800

    Update：页面初始化

---
+++ src/views/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const IndexPage = () => {
+  return (
+    <View>
+      <Text>这里是主页面</Text>
+    </View>
+  );
+};
+
+export default IndexPage;
