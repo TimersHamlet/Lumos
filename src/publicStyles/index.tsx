be1085a9d31908d5bef5d7a5463c410011aa5c04
timers <timershamlet@outlook.com>
2021-03-12 18:03:24 +0800

    Update:播放暂停功能测试

--- src/publicStyles/index.tsx
+++ src/publicStyles/index.tsx
@@ -3,5 +3,6 @@ import {StyleSheet} from 'react-native';
 export const publicStyles = StyleSheet.create({
   textFont: {
     color: '#3377ff',
+    fontSize: 44,
   },
 });
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Button} from 'react-native';
 import {publicStyles} from '../publicStyles/index';
+import Video from 'react-native-video';
 
 const IndexPage = () => {
+  const [playState, setPlayState] = useState(false);
+
   return (
     <View>
+      <Button
+        title={'播放'}
+        onPress={() => {
+          setPlayState(!playState);
+        }}
+      />
+      <Video
+        source={require('../music/testMusic.m4a')}
+        rate={playState ? 1 : 0}
+      />
       <Text style={publicStyles.textFont}>这里是主页面</Text>
     </View>
   );
